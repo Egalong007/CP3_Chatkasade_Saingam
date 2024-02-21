e4e706a2856aa2479f0e15eba0ceac1931c9ceec
Egalong <132530450+Egalong007@users.noreply.github.com>
2024-02-21 22:07:16 +0700

    Create Excercise4_Chatkasade_S.py

---
+++ Excercise4_Chatkasade_S.py
@@ -0,0 +1,14 @@
+Firstname = "Chatkasde"
+Lastname= "Sai-ngam"
+StudentID= "5805012630045"
+FE  = 70.0 #Foundation English
+GB  = 65  #General Business
+ICS = 80  #Introduction to Computer Sysytem
+CP  = 85  #Computer Programming
+print(Firstname,Lastname,StudentID)
+print(" ")
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Sysytem :",ICS)
+print("Computer Programming :",CP)
